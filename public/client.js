ec3d7095a3195f25e4a932c2b513751c6cc3c7a3
Glitch (text-animator) <none>
2018-07-03 18:28:56 +0000

    😳🙌 Checkpoint ./public/client.js:867163/265 ./server.js:867163/516

--- public/client.js
+++ public/client.js
@@ -2,6 +2,14 @@
 var input = document.querySelector('input[name="name"]');
 var output = document.querySelector('.name');
 
+// TextToSVG stuff
+var TextToSVG = require('text-to-svg');
+var fontURL = "https://cdn.glitch.com/df69dc25-ffb4-49de-9faa-129e415fcbac%2Fslkscre.ttf?1530640044868";
+
+// load font
+var textToSVG = TextToSVG.load(fontURL, (err, t2s) => {
+  textToSVG = t2s;
+});
 var textOptions = {fontSize: 70};
 
 let svg; // the svg created through textToSVG
