725d0f37f121ef84d2489544f54b4c200cf19311
Glitch (text-animator-take-2) <none>
2018-07-03 20:17:01 +0000

    🌉🤗 Checkpoint ./animate.js:867163/1 ./views/index.html:867163/538 ./public/animate.js:867163/310

---
+++ public/animate.js
@@ -0,0 +1,11 @@
+import anime from 'animejs';
+
+var lineDrawing = anime({
+  targets: '#lineDrawing .lines path',
+  strokeDashoffset: [anime.setDashoffset, 0],
+  easing: 'easeInOutSine',
+  duration: 1500,
+  delay: function(el, i) { return i * 250 },
+  direction: 'alternate',
+  loop: true
+});
