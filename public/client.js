ed0ecadaa89b3c69d7ae6ef79b719e696c1508bc
Glitch (text-animator) <none>
2018-07-03 18:36:30 +0000

    📇💴 Checkpoint ./public/client.js:867163/50

--- public/client.js
+++ public/client.js
@@ -2,15 +2,15 @@
 var input = document.querySelector('input[name="name"]');
 var output = document.querySelector('.name');
 
-// TextToSVG stuff
-var TextToSVG = require('text-to-svg');
-var fontURL = "https://cdn.glitch.com/df69dc25-ffb4-49de-9faa-129e415fcbac%2Fslkscre.ttf?1530640044868";
+// // TextToSVG stuff
+// var TextToSVG = require('text-to-svg');
+// var fontURL = "https://cdn.glitch.com/df69dc25-ffb4-49de-9faa-129e415fcbac%2Fslkscre.ttf?1530640044868";
 
-// load font
-var textToSVG = TextToSVG.load(fontURL, (err, t2s) => {
-  textToSVG = t2s;
-});
-var textOptions = {fontSize: 70};
+// // load font
+// var textToSVG = TextToSVG.load(fontURL, (err, t2s) => {
+//   textToSVG = t2s;
+// });
+// var textOptions = {fontSize: 70};
 
 let svg; // the svg created through textToSVG
 
@@ -20,6 +20,6 @@ input.oninput = function(event){
   output.textContent = newText;
   
   // attempt to convert text to svg
-  svg = textToSVG.getSVG(newText, textOptions);
-  console.log(svg);  
+  // svg = textToSVG.getSVG(newText, textOptions);
+  // console.log(svg);  
 }
