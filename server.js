e3131b8a59ce60c203d6b2e6f945ee8a74d7d53a
Glitch (text-animator-take-2) <none>
2018-07-03 19:31:30 +0000

    🌄🎄 Checkpoint ./server.js:867163/32 ./public/client.js:867163/402 ./views/index.html:867163/1214

--- server.js
+++ server.js
@@ -5,8 +5,6 @@
 var express = require('express');
 var app = express();
 
-var svgjs = require('svg.js');
-
 // we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
